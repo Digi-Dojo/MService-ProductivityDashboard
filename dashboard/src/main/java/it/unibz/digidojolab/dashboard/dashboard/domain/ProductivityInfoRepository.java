6367e7b945c6332ff26b733f8f8bb193eaa25201
Marco Marinello <me@marcomarinello.it>
2023-08-05 01:27:11 +0200

    Add method to retrieve ProductivityInfo by startupId, activityType and timeframe

--- dashboard/src/main/java/it/unibz/digidojolab/dashboard/dashboard/domain/ProductivityInfoRepository.java
+++ dashboard/src/main/java/it/unibz/digidojolab/dashboard/dashboard/domain/ProductivityInfoRepository.java
@@ -2,5 +2,11 @@ package it.unibz.digidojolab.dashboard.dashboard.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface ProductivityInfoRepository extends JpaRepository<ProductivityInfo, Long> {
+    List<ProductivityInfo> findByStartupIdAndActivityTypeAndTimestampBetween(
+            Long startup_id, String activity_type, LocalDateTime timestamp, LocalDateTime after
+    );
 }
