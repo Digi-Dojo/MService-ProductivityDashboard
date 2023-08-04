7d90e68437c4816558e3f888de3c01863c9d453e
Marco Marinello <me@marcomarinello.it>
2023-08-04 10:20:19 +0200

    Implemented ProductivityInfo repository

---
+++ dashboard/src/main/java/it/unibz/digidojolab/dashboard/dashboard/domain/ProductivityInfoRepository.java
@@ -0,0 +1,6 @@
+package it.unibz.digidojolab.dashboard.dashboard.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductivityInfoRepository extends JpaRepository<ProductivityInfo, Long> {
+}
