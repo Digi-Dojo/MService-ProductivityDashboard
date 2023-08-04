db924072aefb05aafccf0af3c7e2d8e574894c5d
Marco Marinello <me@marcomarinello.it>
2023-08-05 01:27:41 +0200

    First commit of lookup logic

--- dashboard/src/main/java/it/unibz/digidojolab/dashboard/dashboard/application/ProductivityInfoController.java
+++ dashboard/src/main/java/it/unibz/digidojolab/dashboard/dashboard/application/ProductivityInfoController.java
@@ -5,6 +5,11 @@ import it.unibz.digidojolab.dashboard.dashboard.domain.ProductivityInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+
 @RestController
 @RequestMapping("/productivityinfo")
 public class ProductivityInfoController {
@@ -14,5 +19,27 @@ public class ProductivityInfoController {
     @PostMapping
     public ProductivityInfo create(@RequestBody CreateProductivityInfoDTO dto) {
         return mpi.insertPI(dto.getStartup_id(), dto.getTeamMember_id(), dto.getActivity_type());
+    }
+
+    @GetMapping(value="/stats/month/{startup_id}/{yearmonth}")
+    public HashMap<Long, Long> monthStatsPerStartup(@PathVariable("startup_id") String startupId, @PathVariable("yearmonth") String yearMonth) {
+        // find first day of the month as LocalDate object
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMM");
+        YearMonth ym = YearMonth.parse(yearMonth, formatter);
+        LocalDateTime firstDayOfMonth = ym.atDay(1).atStartOfDay();
+        LocalDateTime lastDayOfMonth = firstDayOfMonth.plusMonths(1);
+        System.out.println(firstDayOfMonth.toString());
+        System.out.println(lastDayOfMonth.toString());
+        return mpi.computeWorkedTimeInPeriod(Long.parseLong(startupId), firstDayOfMonth, lastDayOfMonth);
+    }
+
+    @GetMapping(value="/stats/week/{startup_id}/{yearmonthday}")
+    public HashMap<Long, Long> weekStatsPerStartup(@PathVariable("startup_id") String startupId, @PathVariable("yearmonthday") String yearMonthDay) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
+        LocalDateTime firstDayOfMonth = LocalDateTime.parse(yearMonthDay, formatter);
+        LocalDateTime lastDayOfMonth = firstDayOfMonth.plusMonths(1);
+        System.out.println(firstDayOfMonth.toString());
+        System.out.println(lastDayOfMonth.toString());
+        return mpi.computeWorkedTimeInPeriod(Long.parseLong(startupId), firstDayOfMonth, lastDayOfMonth);
     }
 }
--- dashboard/src/main/java/it/unibz/digidojolab/dashboard/dashboard/domain/ManageProductivityInfo.java
+++ dashboard/src/main/java/it/unibz/digidojolab/dashboard/dashboard/domain/ManageProductivityInfo.java
@@ -1,11 +1,21 @@
 package it.unibz.digidojolab.dashboard.dashboard.domain;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 public class ManageProductivityInfo {
     private ProductivityInfoRepository pi_repo;
+
+    @PersistenceContext
+    private EntityManager entityManager;
 
     @Autowired
     public ManageProductivityInfo(ProductivityInfoRepository repo) {
@@ -17,4 +27,33 @@ public class ManageProductivityInfo {
             throw new IllegalArgumentException("Invalid activity_type");
         return pi_repo.save(new ProductivityInfo(startup_id, TeamMember_id, activity_type));
     }
+
+    public HashMap<Long, Long> computeWorkedTimeInPeriod(Long startupId, LocalDateTime start, LocalDateTime end) {
+        List<ProductivityInfo> logins = pi_repo.findByStartupIdAndActivityTypeAndTimestampBetween(
+                startupId, "login", start, end
+        );
+        HashMap<Long, Long> workedMinutes = new HashMap<>();
+        for (int i=0; i<logins.size(); i++) {
+            // ASSUMPTION: a shift can last no longer than 24 hours.
+            // By using this implementation, we can significantly reduce
+            // compute time and also allow edge cases in which the shift
+            // end the night of the first day of the following month.
+            ProductivityInfo pi = logins.get(i);
+            LocalDateTime shiftEnd = pi.getTimestamp().plusDays(1);
+            List<ProductivityInfo> logout = pi_repo.findByStartupIdAndActivityTypeAndTimestampBetween(
+                    startupId, "logout", pi.getTimestamp(), shiftEnd
+            );
+            if (logout.size() < 1)
+                throw new IllegalStateException("Unable to find relevant logout for " + pi);
+            Long shiftDuration = pi.getTimestamp().until(logout.get(0).getTimestamp(), ChronoUnit.MINUTES);
+            System.out.println(pi.getTeamMemberId().toString() + " " + pi.getTimestamp().toString() + " " + logout.get(0).getTimestamp() + " " + shiftDuration);
+            if (workedMinutes.containsKey(pi.getTeamMemberId()))
+                workedMinutes.put(pi.getTeamMemberId(), workedMinutes.get(pi.getTeamMemberId()) + shiftDuration);
+            else
+                workedMinutes.put(pi.getTeamMemberId(), shiftDuration);
+        }
+        return workedMinutes;
+    }
+
+
 }
