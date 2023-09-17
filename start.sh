89ef79a26aafe4cd0ea6af24e3cb07d214ca7534
Marco Marinello <me@marcomarinello.it>
2023-09-17 11:48:16 +0200

    Update start.sh to include new variables

--- start.sh
+++ start.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-java -Dspring.datasource.username=${SPRINGDBUSER} -Dspring.datasource.password=${SPRINGDBPW} -Dspring.datasource.url=jdbc:${SPRINGDBURL} -jar app.jar
+java \
+  -Dspring.datasource.username="${SPRINGDBUSER}" \
+  -Dspring.datasource.password="${SPRINGDBPW}" \
+  -Dspring.datasource.url=jdbc:"${SPRINGDBURL}" \
+  -Dspring.kafka.properties.bootstrap.servers="${KAFKABOOTSTRAPSERVERS}" \
+  -Dspring.kafka.properties.sasl.jaas.config="${KAFKAJAAS}" \
+  -Dspring.kafka.properties.basic.auth.user.info="${KAFKAUSERINFO}" \
+  -Dspring.kafka.properties.schema.registry.url="${SPRINGREGISTRYURL}" \
+  -jar app.jar
 
